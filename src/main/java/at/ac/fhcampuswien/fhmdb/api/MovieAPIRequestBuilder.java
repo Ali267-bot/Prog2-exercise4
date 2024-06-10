5516fdc0c7c5d155ddf17cfb8b287c2d626548b0
clemjberg <clemens.jae@gmail.com>
2024-06-10 09:26:05 +0200

    verschönert

--- src/main/java/at/ac/fhcampuswien/fhmdb/api/MovieAPIRequestBuilder.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/api/MovieAPIRequestBuilder.java
@@ -5,10 +5,12 @@ import at.ac.fhcampuswien.fhmdb.models.Genre;
 public class MovieAPIRequestBuilder {
     private final StringBuilder url;
 
+
     public MovieAPIRequestBuilder(String base) {
         System.out.println("Using API Request Builder!");
         this.url = new StringBuilder(base);
     }
+
 
     public MovieAPIRequestBuilder query(String query) {
         if (query != null && !query.isEmpty()) {
@@ -17,12 +19,14 @@ public class MovieAPIRequestBuilder {
         return this;
     }
 
+
     public MovieAPIRequestBuilder genre(Genre genre) {
         if (genre != null) {
             appendParam("genre", genre.toString());
         }
         return this;
     }
+
 
     public MovieAPIRequestBuilder releaseYear(String releaseYear) {
         if (releaseYear != null) {
@@ -31,12 +35,14 @@ public class MovieAPIRequestBuilder {
         return this;
     }
 
+
     public MovieAPIRequestBuilder ratingFrom(String ratingFrom) {
         if (ratingFrom != null) {
             appendParam("ratingFrom", ratingFrom);
         }
         return this;
     }
+
 
     private void appendParam(String key, String value) {
         if (url.toString().contains("?")) {
@@ -47,8 +53,17 @@ public class MovieAPIRequestBuilder {
         url.append(key).append("=").append(value);
     }
 
+
     public String build() {
         return url.toString();
     }
 }
+
+
+
+
+
+
+
+
 
