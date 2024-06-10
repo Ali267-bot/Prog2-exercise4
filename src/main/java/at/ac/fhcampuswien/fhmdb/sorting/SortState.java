ad6fb8e6ef42999b5c3a0e0e2e680ff96ea7b8e6
clemjberg <clemens.jae@gmail.com>
2024-06-10 09:58:36 +0200

    modified

--- src/main/java/at/ac/fhcampuswien/fhmdb/sorting/SortState.java
+++
@@ -1,9 +0,0 @@
-package at.ac.fhcampuswien.fhmdb.sorting;
-
-import at.ac.fhcampuswien.fhmdb.models.Movie;
-import java.util.List;
-
-public interface SortState {
-    void sort(List<Movie> movies);
-    void setMovieList(List<Movie> movies);
-}
